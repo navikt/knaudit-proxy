b7721b51191126e1e4c1229617b145c43bc3c70e
Paul B. Beskow <paul@beskow.no>
2024-03-01 15:22:25 +0100

    feat(pkg): make backend configurable

---
+++ handler.go
@@ -0,0 +1,74 @@
+package knaudit_proxy
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+)
+
+type Server struct {
+	sender SendCloser
+}
+
+func NewServer(sender SendCloser) *Server {
+	return &Server{
+		sender: sender,
+	}
+}
+
+func jsonResponse(w http.ResponseWriter, msg *Message) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(msg.Code)
+	err := json.NewEncoder(w).Encode(msg)
+	slog.Error("json encoding response", "error", err)
+}
+
+type Message struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Code    int    `json:"code"`
+}
+
+func (h *Server) ReportHandler(w http.ResponseWriter, r *http.Request) {
+	data, err := io.ReadAll(r.Body)
+	if err != nil {
+		jsonResponse(w, &Message{
+			Status:  "bad request",
+			Message: fmt.Errorf("unable to read audit data: %w", err).Error(),
+			Code:    http.StatusBadRequest,
+		})
+
+		return
+	}
+
+	defer func() {
+		_ = r.Body.Close()
+	}()
+
+	err = h.sender.Send(string(data))
+	if err != nil {
+		jsonResponse(w, &Message{
+			Status:  "bad request",
+			Message: fmt.Errorf("storing audit data: %w", err).Error(),
+			Code:    http.StatusInternalServerError,
+		})
+
+		return
+	}
+
+	jsonResponse(w, &Message{
+		Status:  "ok",
+		Message: "audit data stored",
+		Code:    http.StatusOK,
+	})
+}
+
+func (h *Server) StatusHandler(w http.ResponseWriter, _ *http.Request) {
+	jsonResponse(w, &Message{
+		Status:  "ok",
+		Message: "knaudit-proxy is running",
+		Code:    http.StatusOK,
+	})
+}
---
+++ handler_test.go
@@ -0,0 +1,67 @@
+package knaudit_proxy_test
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	knaudit_proxy "github.com/navikt/knaudit-proxy"
+)
+
+type mockSender struct {
+	Err error
+}
+
+func (m mockSender) Send(_ string) error {
+	return m.Err
+}
+
+func (m mockSender) Close() error {
+	return m.Err
+}
+
+func TestHandlers(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name       string
+		handler    http.HandlerFunc
+		expect     string
+		expectCode int
+	}{
+		{
+			name:       "ReportHandler",
+			handler:    knaudit_proxy.NewServer(&mockSender{}).ReportHandler,
+			expect:     `{"status":"ok","message":"audit data stored","code":200}`,
+			expectCode: http.StatusOK,
+		},
+		{
+			name:       "ReportHandler with error",
+			handler:    knaudit_proxy.NewServer(mockSender{Err: fmt.Errorf("oops")}).ReportHandler,
+			expect:     `{"status":"bad request","message":"storing audit data: oops","code":500}`,
+			expectCode: http.StatusInternalServerError,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			w := httptest.NewRecorder()
+			tc.handler.ServeHTTP(w, httptest.NewRequest(http.MethodPost, "/", strings.NewReader("{}")))
+
+			if w.Code != tc.expectCode {
+				t.Errorf("expected status code %d, got %d", tc.expectCode, w.Code)
+			}
+
+			if diff := cmp.Diff(strings.TrimSpace(w.Body.String()), strings.TrimSpace(tc.expect)); diff != "" {
+				t.Errorf("unexpected response (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
